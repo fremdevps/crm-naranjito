5ce7ee0692b8826f819ac94d931c20b520b47498
fremdevps <francisco.escurra21@gmail.com>
2024-05-16 10:58:35 -0500

    chore: Update app menu to display home page and remove folder page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,8 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
   },
+
 ];
