587a32ad118116946478fc37e42c0eed57d2b586
fremdevps <francisco.escurra21@gmail.com>
2024-05-16 12:44:03 -0500

    feat: Add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,9 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage)
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
 
 ];
