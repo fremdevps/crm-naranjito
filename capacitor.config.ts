11b973d12dafe8d7f64489f128c74da18e02fa45
fremdevps <francisco.escurra21@gmail.com>
2024-05-16 10:46:16 -0500

    chore: Update npm dependencies for Capacitor to version 6.0.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ionic.crm',
   appName: 'crm',
-  webDir: 'www'
+  webDir: 'www/browser'
 };
 
 export default config;
