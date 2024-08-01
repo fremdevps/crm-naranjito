10ffe94a436c75df08c66728a7e050387408e1a8
fremdevps <francisco.escurra21@gmail.com>
2024-08-01 12:23:11 -0400

    chore: Update auth service to use HttpClient for API calls

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 }
