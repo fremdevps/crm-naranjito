3c508d6c07bc594647606c2128b60b47b246e939
fremdevps <francisco.escurra21@gmail.com>
2024-09-12 11:38:35 -0400

    refactor: Update auth service to use LoginResponse type in setUser and getUser methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, from, map, Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { LoginParams } from 'src/models/auth.model';
+import { LoginParams, LoginResponse } from 'src/models/auth.model';
 import { Preferences } from '@capacitor/preferences';
 @Injectable({
   providedIn: 'root'
@@ -32,15 +32,15 @@ export class AuthService {
   }
 
 
-  private async setUser(user: any) {
+  private async setUser(user: LoginResponse) {
     //use @capacitor/preferences to store the user token
     await Preferences.set({ key: this.userKey, value: JSON.stringify(user) });
 
   }
-  getUser(): Observable<any> {
+  getUser(): Observable<LoginResponse> {
 
     return from(Preferences.get({ key: this.userKey })).pipe(
-      map((res) => res.value),
+      map((res) => JSON.parse(res.value || '') as LoginResponse),
       catchError((err) => {
         throw err;
       })
@@ -48,5 +48,13 @@ export class AuthService {
 
   }
 
+
+  getToken() {
+    return this.getUser().pipe(
+      map((res) => {
+        return res.items[0].cpassusuar;
+      })
+    );
+  }
 
 }
--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -4,3 +4,20 @@ export interface LoginParams {
   username: string;
   password: string;
 }
+
+
+export interface LoginResponse {
+  items: Item[]
+}
+
+export interface Item {
+  clogiusuar: string
+  cpassusuar: string
+}
+
+// error Response
+export interface ErrorResponse {
+  status: string;
+  error_code: number;
+}
+
